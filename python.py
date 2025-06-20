80700dced2b645694c87129972ad8ae50524d893
aidan <aidenkakkatt@gmail.com>
2025-06-20 14:31:25 +0530

    posneg

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
 a=int(input("enter a number"))
-if a%2==0:
-    print("the number is even")
+if a>0:
+    print("the number is positive")
+elif a<0:
+    print("the number is negative")
 else:
-    print("the number is odd")
+    print("zero")
