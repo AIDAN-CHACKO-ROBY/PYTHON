c8b949a674be7d691ffe46f2c8f203763964eb8a
aidan <aidenkakkatt@gmail.com>
2025-06-20 14:44:28 +0530

    all operations

--- python.py
+++ python.py
@@ -1,7 +1,14 @@
-a=int(input("enter a number"))
-if a>0:
-    print("the number is positive")
-elif a<0:
-    print("the number is negative")
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+print("1:addition, 2:substraction, 3:multiplication, 4:division")
+choice = int(input("enter your choice"))
+if(choice == 1):
+    print(a+b)
+elif(choice == 2):
+    print(a-b)
+elif(choice == 3):
+    print(a*b)
+elif(choice == 4):
+    print(a/b)
 else:
-    print("zero")
+    print("invalid choice")
