9b45fe9b806a594d45e22a4220f9764d01f67459
aidan <aidenkakkatt@gmail.com>
2025-07-10 14:47:12 +0530

    temp

--- temperature.py
+++ temperature.py
@@ -1,3 +1,8 @@
 c=int(input("enter the temp in celcius"))
 f=int(input("enter the temp in fahrenhiet"))
-celcius 
+celcius=(f - 32) * 5 / 9
+fahrenheit=(c * 9 / 5) + 32
+print("temp in celcius:",celcius)
+print("temp in fahrenheit:",fahrenheit)
+print("temp in celcius:", c)
+print("temp in fahrenheit:", f)
